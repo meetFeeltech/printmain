01569a903fc31ca78a4500dc5c27ca7de4b80c3f
MEET-Flutter <114639726+MEET-Flutter@users.noreply.github.com>
2023-03-29 14:41:30 +0530

    13/03/2023 4rd commit

---
+++ lib/model/delete_model.dart
@@ -0,0 +1,15 @@
+class Delete_model {
+  String? message;
+
+  Delete_model({this.message});
+
+  Delete_model.fromJson(Map<String, dynamic> json) {
+    message = json['message'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['message'] = this.message;
+    return data;
+  }
+}
